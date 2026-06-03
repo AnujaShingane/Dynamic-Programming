ee320cf151a6589481d8e20e262133c3826ea111
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-06-03 13:28:23 +0530

    Implement Minimum Path Sum algorithm

---
+++ Grid or 2D/Minimum path Sum.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int minPathSum(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        int[][] dp = new int[n+1][m+1];
+        for(int[] row : dp){
+            Arrays.fill(row,-1);
+        }
+
+        return func(n-1,m-1,grid,dp);
+    }
+
+    public int func(int i, int j , int[][] grid,int[][] dp){
+        if(i==0 && j==0)return grid[i][j];
+        if(i<0 || j<0)return (int)(1e9);
+        if(dp[i][j] != -1)return dp[i][j];
+
+        int up = grid[i][j] + func(i-1,j,grid,dp);
+        int left = grid[i][j] + func(i,j-1,grid,dp);
+
+        return dp[i][j] = Math.min(up,left);
+    }
+}
