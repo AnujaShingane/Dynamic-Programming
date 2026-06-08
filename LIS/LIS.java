21784ba2c5418a0e848745d94fdb3fa17fa7932c
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-06-08 13:03:29 +0530

    Create LIS.java

---
+++ LIS/LIS.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        int[][] dp = new int[n][n+1];
+        for(int[] row : dp){
+            Arrays.fill(row,-1);
+        }
+
+        return func(0,-1,nums,dp);
+    }
+
+    public int func(int ind,int prev,int[] nums,int[][] dp) {
+        if(ind==nums.length)return 0;
+        if(dp[ind][prev+1]!=-1)return dp[ind][prev+1];
+
+        int nottake = func(ind+1,prev,nums,dp);
+        int take = 0;
+        if(prev==-1 || nums[ind]>nums[prev]) take = 1 + func(ind+1,ind,nums,dp);
+
+        return dp[ind][prev+1] = Math.max(take,nottake);
+    }
+}
